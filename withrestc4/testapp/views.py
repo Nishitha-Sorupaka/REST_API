285a59de339f7b4f41976886927b3b1488c4f0bc
Nishitha7395 <132150682+Nishitha7395@users.noreply.github.com>
2024-08-05 14:25:18 +0530

    Updated more files

--- withrestc4/testapp/views.py
+++ withrestc4/testapp/views.py
@@ -3,10 +3,14 @@ from testapp.models import Employee
 from rest_framework.viewsets import ModelViewSet
 from testapp.serializers import EmployeeSerializer
 # Create your views here.
-#from rest_framework.authentication import TokenAuthentication
-#from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
 class EmployeeCRUDViewSetCBV(ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
-    #authentication_classes = [TokenAuthentication]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+    #permission_classes = [DjangoModelPermissions]
+    #permission_classes = [IsAuthenticatedOrReadOnly]
+    #permission_classes = [IsAdminUser]
     #permission_classes = [IsAuthenticated]
